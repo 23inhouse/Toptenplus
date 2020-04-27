8efc9b1080192fa2e836b0cddc65b0644e2663df
Benjamin Lewis <23inhouse@gmail.com>
2020-04-27 17:15:29 +0200

    Replace main interface UI builder with code UI

---
+++ Toptenplus/Toptenplus/Extensions/AppDelegate+gotoAppVC.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate+gotoVC.swift
+//  Toptenplus
+//
+//  Created by Benjamin Lewis on 27/4/20.
+//  Copyright © 2020 Benjamin Lewis. All rights reserved.
+//
+
+import UIKit
+
+extension AppDelegate {
+  func gotoAppVC() {
+    let appViewController = ViewController()
+    window = UIWindow(frame: UIScreen.main.bounds)
+    window?.rootViewController = appViewController
+    window?.makeKeyAndVisible()
+  }
+}
